99354282013688d9270b3c38c58090b543cb3062
ocirebla77 <ocirebla@hotmail.com>
2022-04-20 00:56:03 -0300

    movimentando a camera
    movimentando a camera

---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour {
+    public GameObject jogador;
+    Vector3 distCompensar;
+
+	// Use this for initialization
+	void Start () {
+        distCompensar = transform.position - jogador.transform.position;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = jogador.transform.position + distCompensar;
+		
+	}
+}
