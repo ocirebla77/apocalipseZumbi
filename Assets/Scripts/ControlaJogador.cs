6abac14cec1e70924b209f650550d914d7597e94
ocirebla77 <ocirebla@hotmail.com>
2022-04-20 01:31:51 -0300

    colisao cenario
    colisao cenario

--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ControlaJogador : MonoBehaviour {
     public float Velocidade = 10;
-	
-	// Update is called once per frame
-	void Update () {
+    Vector3 direcao;
+
+
+    // Update is called once per frame
+    void Update () {
 
       float eixoX = Input.GetAxis("Horizontal");
       float eixoZ = Input.GetAxis("Vertical");
-      Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+      direcao = new Vector3(eixoX, 0, eixoZ);
 
-        transform.Translate(direcao * Velocidade * Time.deltaTime);
+        GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position + 
+            (direcao * Velocidade * Time.deltaTime));
+
+    //    transform.Translate(direcao * Velocidade * Time.deltaTime);
 
         if (direcao != Vector3.zero) {
             GetComponent<Animator>().SetBool("Movendo", true);
@@ -23,4 +29,12 @@ public class ControlaJogador : MonoBehaviour {
         }
 		
 	}
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition
+        (GetComponent<Rigidbody>().position +
+        (direcao * Velocidade * Time.deltaTime));
+
+    }
 }
