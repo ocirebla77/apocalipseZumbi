5531549226cbc6f9d08c8e110de1b2663eed3a31
ocirebla77 <ocirebla@hotmail.com>
2022-04-19 23:51:06 -0300

    Movimentação por segundo
    Movimentação por segundo

--- Assets/ControlaJogador.cs
+++ Assets/ControlaJogador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlaJogador : MonoBehaviour {
+    public float Velocidade = 10;
 	
 	// Update is called once per frame
 	void Update () {
@@ -11,7 +12,7 @@ public class ControlaJogador : MonoBehaviour {
       float eixoZ = Input.GetAxis("Vertical");
       Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
 
-        transform.Translate(direcao);
+        transform.Translate(direcao * Velocidade * Time.deltaTime);
 		
 	}
 }
