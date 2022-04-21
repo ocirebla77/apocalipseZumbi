9a1395708b39d4c119b3cde17f4f9784a50b1099
ocirebla77 <ocirebla@hotmail.com>
2022-04-21 01:06:29 -0300

    rotacao zumbi
    rotacao zumbi

--- Assets/Scripts/ControlaInimigo.cs
+++ Assets/Scripts/ControlaInimigo.cs
@@ -21,10 +21,24 @@ public class ControlaInimigo : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        Vector3 direcao = jogador.transform.position - transform.position;
       
 
+        float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+        if (distancia>2.5)
+        {
+            Vector3 direcao = jogador.transform.position - transform.position;
 
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * velocidade * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position +
+                direcao.normalized * velocidade * Time.deltaTime);
+
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+        }
+        else
+        {
+
+        }
     }
 }
