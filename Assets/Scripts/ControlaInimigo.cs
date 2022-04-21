434902a2b347aaf59107b81b34100c63653ed854
ocirebla77 <ocirebla@hotmail.com>
2022-04-20 22:36:25 -0300

    zumbi seguir jogador
    zumbi seguir jogador

---
+++ Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour {
+    public float velocidade =5;
+    public GameObject jogador;
+
+    // Use this for initialization
+    void Start () {
+     //  distanciaCompensar = transform.position - jogador.transform.position;
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 direcao = jogador.transform.position - transform.position;
+      
+
+
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * velocidade * Time.deltaTime);
+    }
+}
